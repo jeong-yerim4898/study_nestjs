d73dda3e344839fa4b5cacaa0b942812a32e5b2a
jeong-yerim4898 <jinju151002@gmail.com>
2024-10-06 21:41:42 +0900

    22:Expose Annotation 사용해보기

--- src/users/entites/users.entity.ts
+++ src/users/entites/users.entity.ts
@@ -7,7 +7,7 @@ import { IsEmail, IsString, Length, ValidationArguments } from "class-validator"
 import { lengthValidationMessage } from "src/common/validation-message/length-validation.message";
 import { stringValidationMessage } from "src/common/validation-message/string-validation-message";
 import { emailValidationMessage } from "src/common/validation-message/email-validation.message";
-import { Exclude } from "class-transformer";
+import { Exclude, Expose } from "class-transformer";
 
 @Entity()
 export class UsersModel extends BaseModel{
@@ -28,6 +28,11 @@ export class UsersModel extends BaseModel{
     // 1. 길이가 20을 넘지 않을 것
     // 2. 유일무이한 값이 될 것
     nickname : string;
+
+    @Expose()
+    get nicknameAndEmail() {
+        return this.nickname + '/' + this.email;
+    }
 
     @Column({
         unique : true
